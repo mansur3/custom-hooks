4be4f5ed31c0e258fcf4ef92f6244d2cc876cb3b
Mansur Dewan <mansurdewan26@gmail.com>
2021-12-13 16:44:09 +0530

    Some change in custom hooks

--- src/utils/useNotification.jsx
+++ src/utils/useNotification.jsx
@@ -1,42 +1,33 @@
-import {useState, useEffect} from "react";
-
-
+import { useState, useEffect } from "react";
 
 export const useNotification = () => {
+  const [success, setSuccess] = useState(true);
+  const [failure, setFailure] = useState(true);
 
-const [success, setSuccess] = useState(true);
-const [failure, setFailure] = useState(true);
-
-
-const successNotification = (message, time) => {
-    if(success){
-        alert(message);
-    }
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setSuccess(false);
-        }, time);
-        return () => clearTimeout(timer);
-    },[])
-}
-
-
-
-const failureNotification = (message, time) => {
-    if(failure) {
-        alert(message);
+  const successNotification = (message, time) => {
+    if (success) {
+      alert(message);
     }
 
-    useEffect(() => {
-        const timera = setTimeout(() => {
-            setFailure(false);
-        }, time)
-        return () => clearTimeout(timera);
-    }, [])
-}
+    const timer = setTimeout(() => {
+      setSuccess(false);
+    }, time);
+    return () => clearTimeout(timer);
+  };
 
+  const failureNotification = (message, time) => {
+    if (failure) {
+      alert(message);
+    }
+    const timera = setTimeout(() => {
+      setFailure(false);
+    }, time);
+    return () => clearTimeout(timera);
+  };
+  useEffect(() => {
+    failureNotification();
+    successNotification();
+  }, []);
 
-return {failureNotification, successNotification};
-
-
-}
+  return { failureNotification, successNotification };
+};
